84dccb69234278b701e26f62491b9e947a1029b3
suryakantsurve@gmail.com <john@gmail.com>
2021-10-05 11:36:08 +0530

    NG

--- src/main/java/com/citius/bootz/controller/ProductController.java
+++ src/main/java/com/citius/bootz/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ public class ProductController {
 	@Autowired
 	private ProductFacade productFacade;
 
+	//@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping(path = "/products",produces = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<List<ProductDTO>> getProducts() {
 		return new ResponseEntity<List<ProductDTO>>(productFacade.getProducts(),HttpStatus.OK);
@@ -36,6 +38,6 @@ public class ProductController {
 		productFacade.save(p);
 		return new ResponseEntity<ProductDTO>(p,HttpStatus.OK);
 
-	}
+	} 
 
 }
